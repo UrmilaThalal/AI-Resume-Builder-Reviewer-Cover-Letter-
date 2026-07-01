907b0799768f520f03b5c16fb468cf8cf2ed9fb1
Urmila Thalal <utsabithalal@gmail.com>
2026-07-01 17:30:32 +0545

    Add pytest for 5 dummy users

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -31,3 +31,47 @@ class AuthenticationTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("access", response.data)
         self.assertIn("refresh", response.data)
+
+    def test_5_dummy_users(self):
+        users = [
+            {
+                "username": "user1",
+                "email": "user1@gmail.com",
+                "password": "User@123"
+            },
+            {
+                "username": "user2",
+                "email": "user2@gmail.com",
+                "password": "User@123"
+            },
+            {
+                "username": "user3",
+                "email": "user3@gmail.com",
+                "password": "User@123"
+            },
+            {
+                "username": "user4",
+                "email": "user4@gmail.com",
+                "password": "User@123"
+            },
+            {
+                "username": "user5",
+                "email": "user5@gmail.com",
+                "password": "User@123"
+            }
+        ]
+
+        for user in users:
+            # Register
+            register_response = self.client.post("/api/register/", user)
+            self.assertEqual(register_response.status_code, status.HTTP_201_CREATED)
+
+            # Login
+            login_response = self.client.post("/api/login/", {
+                "username": user["username"],
+                "password": user["password"]
+            })
+
+            self.assertEqual(login_response.status_code, status.HTTP_200_OK)
+            self.assertIn("access", login_response.data)
+            self.assertIn("refresh", login_response.data)
